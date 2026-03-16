5e0babb3599f811f43c8842fb3db10ab65510bcf
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2026-03-17 00:23:29 +0100

    Add support for swagger and auth via gateway for local development miw-upm/goa#33

--- src/main/java/es/upm/api/configurations/OpenApiConfig.java
+++ src/main/java/es/upm/api/configurations/OpenApiConfig.java
@@ -5,20 +5,24 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
-        info = @Info(title = "Mi API", version = "v1"),
-        security = @SecurityRequirement(name = "bearerAuth")
+        info = @Info(title = "Goa Support API", version = "v1"),
+        security = @SecurityRequirement(name = "oauth2")
 )
 @SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        scheme = "bearer",
-        bearerFormat = "JWT"
+        name = "oauth2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                authorizationCode = @OAuthFlow(
+                        authorizationUrl = "http://localhost:8080/api/goa-user/oauth2/authorize",
+                        tokenUrl = "http://localhost:8080/api/goa-user/oauth2/token"
+                )
+        )
 )
 @Configuration
 public class OpenApiConfig {
-    // empty
 }
-
