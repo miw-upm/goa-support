6441e6ee3fdfa573abae494da7fcb87e834968c6
JBernal <j.bernal@upm.es>
2026-04-23 05:49:56 +0200

    refactored mail. miw-upm/goa#148

--- src/main/java/es/upm/api/domain/services/EmailService.java
+++ src/main/java/es/upm/api/domain/services/EmailService.java
@@ -1,48 +1,49 @@
 package es.upm.api.domain.services;
 
-import es.upm.api.domain.exceptions.BadGatewayException;
 import es.upm.api.domain.model.Email;
+import es.upm.miw.exception.BadGatewayException;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender mailSender;
-
     @Value("${app.mail.from}")
     private String from;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
     public void sendSimple(Email email) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(email.getTo());
-        message.setSubject(email.getSubject());
-        message.setText(email.getBody());
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo(email.getTo());
+            message.setSubject(email.getSubject());
+            message.setText(email.getBody());
+            mailSender.send(message);
+        } catch (MailException e) {
+            throw new BadGatewayException("Error enviando email simple.", e);
+        }
     }
 
     public void sendHtml(Email email) {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = null;
         try {
-            helper = new MimeMessageHelper(message, true, "UTF-8");
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setFrom(from);
             helper.setTo(email.getTo());
             helper.setSubject(email.getSubject());
             helper.setText(email.getBody(), true);
             mailSender.send(message);
-        } catch (MessagingException e) {
-            throw new BadGatewayException("Error de host de email. " + e.getMessage());
+        } catch (MessagingException | MailException e) {
+            throw new BadGatewayException("Error enviando email HTML.", e);
         }
     }
 }
