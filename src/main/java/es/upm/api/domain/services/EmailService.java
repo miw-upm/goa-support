02d67b07bc8d1ded330d7c35bb53c4cf724a1c40
JBernal <j.bernal@upm.es>
2026-05-03 07:18:47 +0200

    added email with attachment. miw-upm/goa#153

--- src/main/java/es/upm/api/domain/services/EmailService.java
+++ src/main/java/es/upm/api/domain/services/EmailService.java
@@ -6,11 +6,16 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -43,6 +48,28 @@ public class EmailService {
             helper.setText(email.getBody(), true);
             mailSender.send(message);
         } catch (MessagingException | MailException e) {
+            throw new BadGatewayException("Error enviando email HTML.", e);
+        }
+    }
+
+    public void sendHtml(Email email, MultipartFile attachment) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom(from);
+            helper.setTo(email.getTo());
+            helper.setSubject(email.getSubject());
+            helper.setText(email.getBody(), true);
+
+            if (attachment != null && !attachment.isEmpty()) {
+                helper.addAttachment(
+                        Objects.requireNonNull(attachment.getOriginalFilename()),
+                        new ByteArrayResource(attachment.getBytes()),
+                        "application/pdf"
+                );
+            }
+            mailSender.send(message);
+        } catch (MessagingException | IOException | MailException e) {
             throw new BadGatewayException("Error enviando email HTML.", e);
         }
     }
--- src/main/java/es/upm/api/infrastructure/resources/EmailResource.java
+++ src/main/java/es/upm/api/infrastructure/resources/EmailResource.java
@@ -5,11 +5,10 @@ import es.upm.api.domain.services.EmailService;
 import es.upm.miw.security.Security;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @PreAuthorize(Security.ADMIN_MANAGER_OPERATOR_URL_TOKEN)
@@ -19,6 +18,7 @@ public class EmailResource {
     public static final String EMAILS = "/emails";
     public static final String SIMPLE = "/simple";
     public static final String HTML = "/html";
+    public static final String ATTACHMENT = "/attachment";
 
     private final EmailService emailService;
 
@@ -30,5 +30,11 @@ public class EmailResource {
     @PostMapping(HTML)
     public void sendHtml(@Valid @RequestBody Email email) {
         this.emailService.sendHtml(email);
+    }
+
+    @PostMapping(value = HTML + ATTACHMENT, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void sendHtmlWithAttachment(@Valid @RequestPart("email") Email email,
+                                       @RequestPart("attachment") MultipartFile attachment) {
+        this.emailService.sendHtml(email, attachment);
     }
 }
